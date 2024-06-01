935791cfe1bd6d955c5737893cea689a7073668e
srivasanth23 <jammulasrivasanth@gmail.com>
2024-06-01 22:16:11 +0530

    backend

--- server/controller/scraperControllers.js
+++
@@ -1,2 +0,0 @@
-
-//"am gl my mz na nb nc nd ne nf ng nh ni nj nk nl nm nn no np nq nr ns nt nu nv nw gb gd ge gg gi bq"
--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const {scarpeFuntion} = require("./functions/scarpeFuntion.js");
 
 const app = express();
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 //middleware
 app.use(bodyParser.json());
